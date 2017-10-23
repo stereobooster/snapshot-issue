764c44b182dcae36ba85e3c5379850ef8452035b
stereobooster <stereobooster@gmail.com>
2017-10-23 12:13:16 +0200

    react-snap

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// just to make sure I'm not confused by cache
+// registerServiceWorker();
